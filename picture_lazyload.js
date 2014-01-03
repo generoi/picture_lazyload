3da9cef5ab1613a9bc567971022054f6687a5429
Oskar Schöldström <public@oxy.fi>
2014-01-03 11:05:15 +0100

    Move spinner logic out of module and fix flexslider/colorbox support

--- picture_lazyload.js
+++ picture_lazyload.js
@@ -1,17 +1,72 @@
 (function($) {
+
+  // Simple lazy load function which triggers the picture load.
+  jQuery.fn.lazyLoad = function() {
+    this.each(function() {
+      var parent = $(this)
+        .attr('data-picture', '') // Set the data-picture property on the picture
+        .removeClass('spinner')
+        .parent()[0]; // Select the parent as picturefill traverses DOM children.
+      window.picturefill(parent);
+    });
+    return this;
+  };
+
   Drupal.behaviors.pictureLazyload = {
     attach: function(context, settings) {
+
+      // Hook into flexslider and force the load.
+      $('.flexslider').bind('start', Drupal.pictureLazyload.flexslider.start);
+      // Dont automatically lazyload flexslider pictures as we manually do it
+      // before rebuilding the slider. This way we know that the images exist.
+      $('.flexslider .picture').addClass('lazyload-manual');
+
       // Ensure we always pass a raw DOM element to picture fill, otherwise it
       // will fallback to the document scope and maybe handle to much.
-      $('.picture', context).once('picture-lazyload').waypoint(function() {
-        $(this).attr('data-picture', '');
-        window.picturefill(this);
-      }, { offset: '100%', triggerOnce: true });
+      $('.picture', context)
+        .not('.lazyload-manual') // Dont load pictures tagged with manual loading.
+        .once('picture-lazyload')
+        .waypoint(function () { $(this).lazyLoad(); }, {
+          offset: '100%', triggerOnce: true
+        });
+
+      if (context === '#cboxLoadedContent') {
+        // Wrap colorbox iframes in .flex-video
+        $('iframe', context).once('colorbox-flex-video').wrap('<div class="flex-video"></div>');
+      }
+
       Drupal.pictureLazyload.pictureColorbox(context);
     }
   };
 
   Drupal.pictureLazyload = Drupal.pictureLazyload || {};
+
+  Drupal.pictureLazyload.flexslider = {
+    start: function(e) {
+      var $slider = $(e.target)
+        , slider = $slider.data('flexslider');
+
+      $slider
+        .find('.picture')
+        .once('picture-lazyload')
+        .lazyLoad();
+
+      // Rebuild the slider.
+      slider.setup();
+
+      // Add .colorbox-iframe links which works like .colorbox-load but with
+      // preconfigured values. This needs to run after the flexslider has been
+      // initialized.
+      $slider.find('.colorbox-iframe')
+        .once('init-colorbox-load', function() {
+          $(this).colorbox($.extend({}, Drupal.settings.colorbox, {
+            iframe: true,
+            innerWidth: 800,
+            innerHeight: 400
+          }));
+        });
+    }
+  };
 
   // Copied from picture.js
   Drupal.pictureLazyload.pictureColorbox = function(context) {
@@ -22,14 +77,17 @@
       // Try to resize right away.
       $.colorbox.resize();
       // Make sure the colorbox resizes always when the image is changed.
-      $('img', context).once('colorbox-lazy-load', function(){
-        $(this).load(function(){
+      $('img', context).once('colorbox-lazy-load', function() {
+        $(this).load(function() {
           // Ensure there's no max-width / max-height otherwise we won't get
           // the proper values. We could use naturalWeight / naturalHeight
           // but that's not supported by <IE9 and Opera.
           this.style.maxHeight = $(window).height() + 'px';
           this.style.maxWidth = $(window).width() + 'px';
-          $.colorbox.resize({innerHeight: this.height, innerWidth: this.width});
+          $.colorbox.resize({
+            innerHeight: this.height,
+            innerWidth: this.width
+          });
           // Remove overwrite of this values again to ensure we respect the
           // stylesheet.
           this.style.maxHeight = null;
@@ -39,4 +97,3 @@
     }
   };
 }(jQuery));
-
